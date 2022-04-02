224fcc124ac97a26e6624c7b8276e23173a3d885
Eljas Jalo <eljasjal@gmail.com>
2022-04-02 19:21:20 +0300

    Add alert method and remove unused methods

--- ios/Plugin/KlarnaLogging.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import KlarnaMobileSDK
-
-public struct KlarnaLogging {
-    var off = KlarnaLoggingLevel.off
-    var error = KlarnaLoggingLevel.error
-    var verbose = KlarnaLoggingLevel.verbose
-}
